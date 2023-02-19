5141413848cc86b1c7a6fe31cb9a92e2e7630c0b
SaraEOlivera <saraeolivera@gmail.com>
2023-02-19 01:12:31 -0300

    Admite numeros de hasta 6 cifras

--- 7. Number to Words.py
+++ 7. Number to Words.py
@@ -3,30 +3,45 @@ import random
 def number_to_words(numero:int) -> str:
     if numero == 0:
         return 'Cero'
+    elif numero == 1000:
+        return 'Mil'
     en_palabras= ''
     unidades =['', 'uno','dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve']
     decenas = ['', 'dieci', 'veinti', 'treinta y ', 'cuarenta y ', 'cincuenta y ', 'sesenta y ', 'setenta y ', 'ochenta y ', 'noventa y ']
     centenas = ['', 'ciento', 'doscientos', 'trescientos', 'cuatrocientos', 'quinientos', 'seiscientos', 'setecientos', 'ochocientos', 'novecientos']
+    unidades_mil = unidades
+    decenas_mil = decenas
+    centenas_mil = centenas
     
-    # descomponer el numero. Hay que completar con 0 a la izquierda segun cantidad digitos
-    # la 1° expresion rellena con 0 si hay menos de 3 digitos y a esto le agrega el numero
-    numero = '0' * (3-len(str(numero))) + str(numero)
+    numero = '0' * (6-len(str(numero))) + str(numero)
     
-    # Convertir cada digito str a int:
+    
     unidad = int(numero[-1])
     decena = int(numero[-2])
     centena = int(numero[-3])
+    unidad_mil = int(numero[-4])
+    decena_mil = int(numero[-5])
+    centena_mil = int(numero[-6])
     
-    # Strip saca espacio en blanco al tener 2 o 1 digito
-    en_palabras = '{} {}{}'.format(centenas[centena], decenas[decena], unidades[unidad]).strip()
+    # Strip saca espacio en blanco al tener menos digitos
+    en_palabras = '{} {} {}{}'.format(unidades_mil[unidad_mil], centenas[centena], decenas[decena], unidades[unidad]).strip()
     
+    if len(numero) == 4:
+        en_palabras = '{} mil {} {}{}'.format(unidades_mil[unidad_mil], centenas[centena], decenas[decena], unidades[unidad]).strip()
+    elif len(numero) == 5:
+        en_palabras = '{}{} mil {} {}{}'.format(decenas_mil[decena_mil], unidades_mil[unidad_mil], centenas[centena], decenas[decena], unidades[unidad]).strip()
+    elif len(numero) == 6:
+        en_palabras = '{} {}{} mil {} {}{}'.format(centenas_mil[centena_mil],decenas_mil[decena_mil] ,unidades_mil[unidad_mil], centenas[centena], decenas[decena], unidades[unidad]).strip()
     
+ 
     #casos especiales a cambiar:
     en_palabras = en_palabras.replace('dieciuno', 'once')
     en_palabras = en_palabras.replace('diecidos', 'doce')
     en_palabras = en_palabras.replace('diecitres', 'trece')
     en_palabras = en_palabras.replace('diecicuatro', 'catorce')
     en_palabras = en_palabras.replace('diecicinco', 'quince')
+    en_palabras = en_palabras.replace('uno mil', 'un mil')
+
     
     #cuando termina en:
     if en_palabras.endswith('dieci'):
@@ -42,7 +57,7 @@ def number_to_words(numero:int) -> str:
     
     return en_palabras.capitalize() #imprime en mayuscula la primera letra
 
-
-valor = random.randint(0, 999)
+#valor = 451587
+valor = random.randint(0, 999999)
 print(valor , "se escribe:", number_to_words(valor))
     
